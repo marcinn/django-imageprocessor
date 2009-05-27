15e791acb0c0e8dd6bcf0e146f2eedf03e96e67f
marcin <marcin@myhost.localdomain>
2009-05-27 23:15:28 +0200

    better impl of thumbnail_url tag

--- src/netwizard/imageprocess/templatetags/imageprocess.py
+++ src/netwizard/imageprocess/templatetags/imageprocess.py
@@ -1,13 +1,44 @@
 from netwizard.imageprocess import helpers 
-from django.template import Library, Node
+from django.template import Library, Node, TemplateSyntaxError
 from django.conf import settings
 import os
 
 register = Library()
 
-@register.simple_tag
-def thumbnail_url(file, width, height):
-    file = str(file)
-    if not os.path.isabs(file):
-        file = os.path.join(settings.MEDIA_ROOT, file)
-    return helpers.thumbnail_url(file, (int(width),int(height)))
+class ThumbnailUrlNode(Node):
+    def __init__(self, path, size, options):
+        self.path = path
+        self.size = size
+        self.options = options
+
+    def render(self, context):
+        resolved_options = dict(zip(self.options.keys(), [self.options[v].resolve(context) for v in self.options]))
+        size = [int(z.resolve(context)) for z in self.size]
+        path = str(self.path.resolve(context))
+        if not os.path.isabs(path):
+            path = os.path.join(settings.MEDIA_ROOT, path)
+        return helpers.thumbnail_url(path, size)
+
+
+@register.tag(name='thumbnail_url')
+def thumbnail_url(parser, token):
+    bits = token.contents.split(' ')
+    if len(bits) < 4:
+        raise TemplateSyntaxError("'%s' takes at least two arguments"
+                                  " (image path, width, height)" % bits[0])
+    file, width, height = [parser.compile_filter(bit) for bit in bits[1:4]]
+    options = {}
+
+    if len(bits) > 4:
+        bits = iter(bits[4:])
+        for bit in bits:
+            for arg in bit.split(","):
+                if '=' in arg:
+                    k, v = arg.split('=', 1)
+                    k = k.strip()
+                    options[k] = parser.compile_filter(v)
+                elif arg:
+                    raise TemplateSyntaxError("Unhandled parameter: %s" % bit)
+
+    return ThumbnailUrlNode(file, (width, height), options)
+

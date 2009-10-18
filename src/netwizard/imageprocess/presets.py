16fc80861705f59b2613121cd14d40a4ac32b41b
Marcin Nowak <marcin@dracoarc.localdomain>
2009-10-19 00:12:27 +0200

    more docstrings

--- src/netwizard/imageprocess/presets.py
+++ src/netwizard/imageprocess/presets.py
@@ -6,29 +6,51 @@ author: Marcin Nowak (marcin.j.nowak@gmail.com)
 import os
 
 class Preset(object):
-    def __init__(self, image_class, filters=None, output_dir=None, file_pattern=None, quality=None):
+    """
+    Preset instance represents image rendering settings
+    and optional output place
+    """
+
+    def __init__(self, image_class, filters=None, \
+            output_dir=None, file_pattern=None, quality=None):
+        """
+        initializes preset with specified image class,
+        filters set, quality, and output file settings
+        """
         self.filters = filters
         self.image_class = image_class
         self.output_dir = output_dir
         self.file_pattern = file_pattern
         self.quality = quality
 
+    def __repr__(self):
+        return '[%s], %d filters, output=%s' % (self.image_class,
+                len(self.filters), os.path.join(self.output_dir,
+                    self.file_pattern))
+            
 
-def get_preset_image(file, preset_name):
+
+def get_preset_image(filename, preset_name):
+    """
+    returns image instance created from file and
+    prepared using preset, which is registered 
+    by name
+    """
     import settings
     preset = library.get(preset_name)
-    image = preset.image_class(filename=file, quality=preset.quality)
-    for filter, args, options in preset.filters:
-        if isinstance(filter, str):
-            getattr(image, filter)(*args, **options)
+    image = preset.image_class(filename=filename, quality=preset.quality)
+    for filter_instance, args, options in preset.filters:
+        if isinstance(filter_instance, str):
+            getattr(image, filter_instance)(*args, **options)
         else:
-            image.process(filter, *args, **options)
+            image.process(filter_instance, *args, **options)
     filename, ext = os.path.splitext(os.path.split(image.filename)[1])
-    pattern_args = {'filename': filename, 'ext': ext,}
+    pattern_args = {'filename': filename, 'ext': ext}
 
     outfilename = preset.file_pattern or '%(filename)s%(ext)s' 
     outfilename = outfilename % pattern_args
-    outdir = preset.output_dir or (os.path.join(settings.PRESETS_ROOT, preset_name))
+    outdir = preset.output_dir or (os.path.join(settings.PRESETS_ROOT, 
+        preset_name))
     if not os.path.isdir(outdir):
         os.makedirs(outdir, 0777)
     image.preset_name = preset_name
@@ -39,26 +61,50 @@ def get_preset_image(file, preset_name):
 
 
 class Library(object):
+    """
+    Class that represents presets registry
+    """
     def __init__(self):
+        """
+        initializes presets registry instance
+        """
         self.presets = {}
 
     def register(self, name, preset):
+        """
+        register preset instance by name
+        """
         if self.presets.has_key(name):
             raise KeyError('Preset with name "%s" already registered' % name)
         self.presets[name] = preset
 
     def register_once(self, name, preset):
+        """
+        Register preset instance by name.
+        If preset is already registered,
+        registration will be skipped without
+        exception.
+        """
         try:
             self.register(name, preset)
         except KeyError:
             pass
 
     def unregister(self, name):
+        """
+        Removes preset from registry
+        """
         self.presets.pop(name)
 
     def get(self, name):
+        """
+        Access preset instance from registry
+        by name
+        """
         return self.presets[name]
 
+
+# library represents default presets registry
 
 library = Library()
 

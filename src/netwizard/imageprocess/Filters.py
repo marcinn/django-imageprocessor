967c21a3da63ce4a4ef9a234cc47448fd73bf5aa
Marcin Nowak <marcin@dracoarc.localdomain>
2009-10-18 15:08:09 +0200

    added watermark filter

--- src/netwizard/imageprocess/Filters.py
+++ src/netwizard/imageprocess/Filters.py
@@ -34,5 +34,28 @@ Image.register_filter('composite', ImageChops.composite)
 
 # custom filters
 
-def watermark(image, watermark, position=('right', 'bottom'), **kwargs):
-    pass
+def watermark(im, mark, position=('right','bottom',), opacity=1.0):
+    """
+    applies watermark onto image
+    im - destination image PIL.Image instance
+    mark - watermark image PIL.Image instance
+    position - (xpos, ypos) tuple, where:
+        xpos: left, right, center
+        ypos: top, bottom, center 
+    opacity - 0..1
+    """
+    layer = _pImage.new('RGBA', im.size, (0,0,0,0))
+
+    _xpos = {'left': 0, 'right': im.size[0]-mark.size[0], 'center': (im.size[0]-mark.size[0])/2,}
+    _ypos = {'top': 0, 'bottom': im.size[1]-mark.size[1], 'center': (im.size[1]-mark.size[1])/2,}
+    
+    try:
+        x, y = _xpos[position[0]], _ypos[position[1]]
+    except KeyError:
+        raise RuntimeError('Invalid watermark position placement: %s' % str(position))
+
+    layer.paste(mark, (x,y))
+    return _pImage.composite(layer, im, layer)
+       
+
+Image.register_filter('watermark', watermark)
